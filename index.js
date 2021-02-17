2c7a50b4680b878be6ac935abd545f28c34a4e09
Aarti Arya <aartiarya0485@gmail.com>
2021-02-17 21:29:44 +0530

    third commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./config/mongoose');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 3000;
 
 //Setting middleware
 app.use(express.urlencoded({ extended: true }));
